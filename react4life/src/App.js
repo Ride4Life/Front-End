c9faae919d68bf402dde99d26e0cabea811722e6
Tatyana Dzhura <tatyanadzhura@gmail.com>
2020-01-30 21:22:36 -0500

    Driver Register form in process

--- react4life/src/App.js
+++ react4life/src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDom from 'react-dom';
+import DriverRegister from "./components/DriverRegister";
 import './App.css';
 
-function App() {
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Register />
     </div>
-  );
+  )
 }
 
 export default App;
+ 
