ff31cffdb6d595833c655cc974f57d20db0d8769
brendantfinn <brendantfinn@gmail.com>
2020-02-07 19:24:32 -0600

    moved review component to private route of /review

--- src/App.js
+++ src/App.js
@@ -9,16 +9,20 @@ import './App.css';
 import RideRequesterForm from './components/RideRegisterForm'
 import FormikUserLoginForm from './components/UserLoginForm';
 import PrivateRoute from "./components/PrivateRoute";
+
 import { Nav } from "./components/Nav"
+import ReviewForm from './components/ReviewForm';
 function App() {
   return (
     <Router>
       <div className="App">
         <Nav />
+        {/* <ReviewForm /> */}
         <Switch>
           <Route exact path="/" />
           <Route path="/login" component={FormikUserLoginForm} />
           <Route path="/register" component={RideRequesterForm} />
+          <Route path="/review" component={ReviewForm} />
           {/* <PrivateRoute path="/driver-dashboard" component={DriverDashboard} />
           <PrivateRoute path="/rider-dashboard" component={RiderDashboard} /> */}
         </Switch>
